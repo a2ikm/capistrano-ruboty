690e2d77166f26bbcc78423047e97bc09d680e86
Masato Ikeda <masato.ikeda@gmail.com>
2016-11-27 13:25:22 +0900

    Initial commit

---
+++ lib/capistrano/ruboty.rb
@@ -0,0 +1,7 @@
+require "capistrano/ruboty/version"
+
+module Capistrano
+  module Ruboty
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/ruboty/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Ruboty
+    VERSION = "0.1.0"
+  end
+end
